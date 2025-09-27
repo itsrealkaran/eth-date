cab6baee284490e080ede06fe7d2a2103e6281ef
itsrealkaran <karansingh@duck.com>
2025-09-27 15:33:43 +0530

    tragic: Project Change

--- scripts/start-ngrok.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting ngrok tunnel for your Next.js app..."
-echo ""
-echo "Make sure your Next.js app is running on port 3000"
-echo "You can start it with: pnpm dev"
-echo ""
-echo "Starting ngrok..."
-ngrok http 3000
